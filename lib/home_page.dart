8142d3c7b3bc15730971271e690225402f8d0764
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-15 01:53:15 +0700

    ADD - Import + Permission request

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class FullPage extends StatefulWidget {
   const FullPage({super.key});
@@ -8,18 +10,21 @@ class FullPage extends StatefulWidget {
 }
 
 class _FullPageState extends State<FullPage> {
+  File? _imageFile;
+
+  Future<void> _requestPermissions() async {
+    await Permission.camera.request();
+    await Permission.storage.request();
+    await Permission.manageExternalStorage.request();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
-        actions: const [],
-      ),
+      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-            children: [],
-        ),
+        child: Column(children: []),
       ),
     );
   }
