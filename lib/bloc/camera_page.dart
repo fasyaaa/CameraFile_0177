85bad090b90d9aaf42f644de68783acbb010e1e7
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-15 02:36:44 +0700

    ADD - stfl camera page

---
+++ lib/bloc/camera_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+            children: [],
+        ),
+      ),
+    );
+  }
+}
